c4c68d093c37f2924592241e9ab37dc83aa59454
webmasteriqbal <webmasteriqbal@users.noreply.github.com>
2021-02-27 12:59:05 +0000

    Fix styling

--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -4,11 +4,9 @@ namespace Webmasteriqbal\UnitConversions\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Webmasteriqbal\UnitConversions\Temperature;
-use Webmasteriqbal\UnitConversions\Wight;
 
 class TemperatureTest extends TestCase
 {
-
     /** @test */
     public function it_can_convert_celsius_to_fahrenheit()
     {
